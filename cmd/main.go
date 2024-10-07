419e9b6a392480487649cb7f815eddc77183452c
william <william-cullum@hotmail.co.uk>
2024-10-07 13:26:55 +0100

    old copy pasta and this will work with the c server when you start it.. if the c server worked for me that is

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
 	"net"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	c "farkle/common"
+	client "farkle/pkg/client"
+	view "farkle/pkg/view"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/log"
@@ -92,10 +99,10 @@ func (a *app) Start() {
 }
 
 func (a *app) ProgramHandler(s ssh.Session) *tea.Program {
-	m := initialModel()
+	m := InitialBaseModel()
 	m.app = a
-	m.playerName = s.User()
-	m.players = append(m.players, s.User())
+	// m.playerName = s.User() ** These need changing to be passing game state into the ui object instead
+	// m.players = append(m.players, s.User())
 
 	p := tea.NewProgram(m, bubbletea.MakeOptions(s)...)
 	a.progs = append(a.progs, p)
@@ -103,51 +110,176 @@ func (a *app) ProgramHandler(s ssh.Session) *tea.Program {
 	return p
 }
 
-type (
-	ReadyMsg struct {
-		msg string
-	}
-)
+type GameClient interface {
+	Connect(addr string) error
+	Read() (<-chan []byte, <-chan error) // returns channel holding state of the game or error
+	Respond([]byte) error                // writes a response to the server - 1 to play atm
 
-type model struct {
+}
+
+type tcpResponse []byte
+
+type tcpReadError string
+
+// Base model will hold the UI and Game client. It will be monitored within bubbletea loop
+// and call ui and game methods based on user input
+
+type BaseModel struct {
 	*app
-	playerName string
-	players    []string
+	log        *log.Logger
+	client     GameClient
+	UI         *view.UI // this naming is horrible :)
+	Display    string
+	GameData   <-chan []byte
+	GameErrors <-chan error
 }
 
-func initialModel() model {
-	return model{players: []string{}}
+func InitialBaseModel() *BaseModel {
+	return &BaseModel{
+
+		client: client.NewClient(),
+		UI:     view.NewUI(),
+	}
 }
 
-func (m model) Init() tea.Cmd {
+type ConnectionSuccess struct{}
+
+// currently this will get stuck on connection failed and like not listen for a new connection or
+// something - not sure if its to do with ui state conditional within the update function
+type ConnectionFailed struct{ err string }
+
+func (m *BaseModel) AttemptConnection(addr string) tea.Msg {
+	err := m.client.Connect(addr)
+	if err != nil {
+		return ConnectionFailed{err: err.Error()}
+	}
+	return ConnectionSuccess{}
+}
+
+type FailedRespondingToServer struct{}
+type SuccessfulResponse struct{}
+
+func (m *BaseModel) sendResponse() tea.Cmd {
+	return func() tea.Msg {
+
+		err := m.client.Respond([]byte{'1'})
+		if err != nil {
+			return FailedRespondingToServer{}
+		}
+		return SuccessfulResponse{}
+	}
+
+}
+
+func (m *BaseModel) monitorChannels() tea.Cmd {
+
+	return func() tea.Msg {
+		select {
+		case data := <-m.GameData:
+			return tcpResponse(data)
+		case err := <-m.GameErrors:
+			return tcpReadError(err.Error())
+		}
+	}
+
+}
+
+func (m *BaseModel) Init() tea.Cmd {
 	return nil
 }
 
-// Update processes incoming messages and updates the model state accordingly
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m *BaseModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "q", "ctrl+c":
+		case "q", "ctrl+c", "esc":
 			return m, tea.Quit
-		case "r":
-			m.app.send(ReadyMsg{msg: m.playerName})
+		case "c":
+			if m.UI.CurrState == view.WelcomeState {
+				// Pass the returned message to the Bubble Tea framework
+				return m, func() tea.Msg {
+					return m.AttemptConnection("localhost:4121")
+				}
+			}
+		case "1":
+			if m.UI.CurrState == view.SuccessfulConnection {
+			}
 		}
-	case ReadyMsg:
-		m.players = append(m.players, msg.msg)
 		return m, nil
+	case ConnectionFailed:
+		m.UI.CurrState = view.FailedConnection
+		return m, nil
+	case ConnectionSuccess:
+		m.UI.CurrState = view.SuccessfulConnection
+		m.GameData, m.GameErrors = m.client.Read()
+		return m, m.monitorChannels()
+
+	case FailedRespondingToServer:
+
+	case tcpResponse:
+		var gs c.GameData
+		r := bytes.NewReader(msg)
+		if err := json.NewDecoder(r).Decode(&gs); err != nil {
+			fmt.Print("do something")
+		}
+		m.UI.Data = gs
+		m.UI.CurrState = view.GameLive
+		return m, m.monitorChannels()
+
 	}
+
 	return m, nil
 }
 
-func (m model) View() string {
-	s := ""
-	for _, player := range m.players {
-		s += player
-	}
-
-	return s
+func (m *BaseModel) View() string {
+	// m.log.Print(m.screen)
+	return m.UI.Render()
 }
+
+// type (
+// 	ReadyMsg struct {
+// 		msg string
+// 	}
+// )
+
+// type model struct {
+// 	*app
+// 	playerName string
+// 	players    []string
+// 	view       *view.UI
+// }
+
+// func initialModel() model {
+// 	return model{
+// 		players: []string{},
+// 		view:    view.NewUI(),
+// 	}
+// }
+
+// func (m model) Init() tea.Cmd {
+// 	return nil
+// }
+
+// // Update processes incoming messages and updates the model state accordingly
+// func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+// 	switch msg := msg.(type) {
+// 	case tea.KeyMsg:
+// 		switch msg.String() {
+// 		case "q", "ctrl+c":
+// 			return m, tea.Quit
+// 		case "r":
+// 			m.app.send(ReadyMsg{msg: m.playerName})
+// 		}
+// 	case ReadyMsg:
+// 		m.players = append(m.players, msg.msg)
+// 		return m, nil
+// 	}
+// 	return m, nil
+// }
+
+// func (m model) View() string {
+// 	return m.view.Render()
+// }
 
 func main() {
 
